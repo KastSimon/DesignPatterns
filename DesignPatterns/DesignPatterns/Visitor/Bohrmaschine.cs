b1d08bf69f93849afccd6343249de9d38a9a130f
Bülent <buelent@rsnweb.ch>
2019-12-13 13:06:50 +0100

    Revert "Revert "Add Visitor""
    This reverts commit 3e197bdd6991e8241a7f21a8a2994855c070be73.

---
+++ DesignPatterns/DesignPatterns/Visitor/Bohrmaschine.cs
@@ -0,0 +1,10 @@
+﻿namespace DesignPatterns.Visitor
+{
+    public class Bohrmaschine: IProdukt {
+        public double Wert { get; set; }
+
+        public void Accept(IVisitor visitor) {
+            visitor.Visit(this);
+        }
+    }
+}
---
+++ DesignPatterns/DesignPatterns/Visitor/DiscountedPriceTotal.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace DesignPatterns.Visitor
+{
+    class DiscountedPriceTotal: IVisitor {
+        private double summe;
+        public void Visit(GartenStuhl g) {
+            summe += g.Wert;
+        }
+
+        public void Visit(Bohrmaschine b) {
+            summe += b.Wert;
+            Console.WriteLine(summe);
+        }
+    }
+}
