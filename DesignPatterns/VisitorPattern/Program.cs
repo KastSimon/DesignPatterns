c4929c1fd12f811cc1667a21e970085514fe875f
Bülent <buelent@rsnweb.ch>
2019-12-16 21:32:21 +0100

    Add Console

---
+++ DesignPatterns/VisitorPattern/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using DesignPatterns.Visitor;
+
+namespace VisitorPattern {
+    class Program {
+        static void Main(string[] args) {
+            var x = new VisitorMain();
+            x.Main();
+            Console.ReadKey();
+        }
+    }
+}
